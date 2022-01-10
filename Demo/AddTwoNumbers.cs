e72c1f0ec30ce8500c5eb88f4ade2ec4519195ca
srikantheddula <you@example.com>
2022-01-10 14:15:31 +0530

    [srikantheddula]:mesg Adding Two Numbers

---
+++ Demo/AddTwoNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo
+{
+    internal class AddTwoNumbers
+    {
+       
+       public static void Addition()
+           {
+                     int sum;
+                     Console.WriteLine(" enter value for a:");
+                     int a = Convert.ToInt32(Console.ReadLine());
+                     Console.WriteLine(" enter value for b:");
+                     int b = Convert.ToInt32(Console.ReadLine());
+
+                    sum = a + b;
+                    Console.WriteLine("\n"+sum);
+            }
+      }     
+ }
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -7,6 +7,7 @@ namespace Demo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            AddTwoNumbers.Addition();
         }
     }
 }
